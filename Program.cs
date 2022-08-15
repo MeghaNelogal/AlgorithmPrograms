f8fcce8a826753997227a9c64a59988e53a69e4e
Your Name <you@example.com>
2022-08-15 21:14:16 +0530

    [Megha] Add: Insertion Sort Done

--- Program.cs
+++ Program.cs
@@ -7,26 +7,35 @@ namespace AlgotithmPrograms
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Algotithm Programs Problems");
-            Console.WriteLine("1.Binary Serach Program\n2.Exit the program");
+            Console.WriteLine("1.Binary Serach Program\n2.Insertion sort program");
+            const string File_Path = @"D:\BridgelabzPracticeProblems\AlgorithmPrograms\AlgorithmPrograms\AlgorithmPrograms\AlgorithmPrograms\Data.txt";
             bool check = true;
             while (check)
             {
                 Console.WriteLine("Enter above option to execute");
+                Algorithm search = new Algorithm();
+                string words = File.ReadAllText(File_Path);
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
-                        const string File_Path = @"D:\BridgelabzPracticeProblems\AlgorithmPrograms\AlgorithmPrograms\AlgorithmPrograms\AlgorithmPrograms\Data.txt";
-                        string words = File.ReadAllText(File_Path);
+
+
                         string[] arr = words.Split(',');
                         Console.WriteLine("Enter word to Search");
                         string a = Console.ReadLine();
-                        Algorithm search = new Algorithm();
+
                         int result = search.BinarySearch(arr, a);
                         if (result == -1)
                             Console.WriteLine("Element not present");
                         else
                             Console.WriteLine("Element found at " + result);
+                        break;
+                    case 2:
+                        string[] arr2 = words.Split(',');
+                        InsertionSort problem = new InsertionSort();
+                        problem.Insertion(arr2);
+                        problem.Display(arr2);
                         break;
                     default:
                         check = false;
